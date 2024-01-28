28d9c0b364c3d36e2a8a1270e40dc85d23b1ef22
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-29 01:45:40 +0800

    simple GUI for the program

--- facial_recogntion.py
+++ facial_recogntion.py
@@ -1,2 +1,14 @@
 #FINAL PROJECT: Facial Recognition 
 # https://www.youtube.com/watch?v=z_dbnYHAQYg&t=461s
+
+import tkinter as tk
+import customtkinter as ctk
+
+class App:
+    def __init__(self):
+        pass
+    def start(self):
+        pass
+if __name__ == "__main__":
+    app = App()
+    app.start()
