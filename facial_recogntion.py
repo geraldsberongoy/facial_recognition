5fc5b5ce2157ffdf92b614037fee269cec3ef21b
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-29 01:49:39 +0800

    creating window

--- facial_recogntion.py
+++ facial_recogntion.py
@@ -6,9 +6,11 @@ import customtkinter as ctk
 
 class App:
     def __init__(self):
-        pass
+        self.main_window = ctk.CTk()
+        self.main_window.geometry("1200x520+350+100")
+        self.main_window.title("Facial Recognition")
     def start(self):
-        pass
+        self.main_window.mainloop()
 if __name__ == "__main__":
     app = App()
     app.start()
